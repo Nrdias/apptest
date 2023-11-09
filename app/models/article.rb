886e9829886de8c823e1b81a23aa8cff93a7ecee
Nrdias <natandias.dev@gmail.com>
2023-11-09 17:56:31 -0300

    article mvc

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
+# Article class
 class Article < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 5, maximum: 10 }
+  validates :description, presence: true, length: { minimum: 3, maximum: 3 }
 end
